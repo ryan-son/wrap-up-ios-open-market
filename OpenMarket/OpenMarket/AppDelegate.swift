111830f99f648d4bf5d41786309ffe24b1ee0d6a
ryan-son <ryan.son1002@gmail.com>
2021-08-24 17:38:29 +0900

    feat: Storyboard 삭제 및 Window 설정
    Fixes: #1

--- OpenMarket/OpenMarket/AppDelegate.swift
+++ OpenMarket/OpenMarket/AppDelegate.swift
@@ -10,13 +10,25 @@ import UIKit
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
+
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        if #available(iOS 13.0, *) {
+            print("Started up with scene based lifecycle")
+        } else {
+            print("Started up with app based lifecycle")
+            window = UIWindow(frame: UIScreen.main.bounds)
+            let viewController = ViewController()
+            window?.rootViewController = viewController
+            window?.makeKeyAndVisible()
+        }
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication,
                      configurationForConnecting connectingSceneSession: UISceneSession,
                      options: UIScene.ConnectionOptions) -> UISceneConfiguration {
--- OpenMarket/OpenMarket/SceneDelegate.swift
+++ OpenMarket/OpenMarket/SceneDelegate.swift
@@ -14,6 +14,10 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene,
                willConnectTo session: UISceneSession,
                options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let viewController = ViewController()
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
     }
 }
