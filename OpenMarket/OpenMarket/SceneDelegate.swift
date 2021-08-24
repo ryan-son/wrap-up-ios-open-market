3e93eb25c221bdf355366fa49687094f1d81f88b
ryan-son <ryan.son1002@gmail.com>
2021-08-24 17:15:38 +0900

    feat: 프로젝트 추가 및 SwiftLint 적용
    Fixes: #1

---
+++ OpenMarket/OpenMarket/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  OpenMarket
+//
+//  Created by Ryan-Son on 2021/08/24.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
---
+++ OpenMarket/OpenMarket/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  OpenMarket
+//
+//  Created by Ryan-Son on 2021/08/24.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
