bb735f4b14688a22f3034cb85c921c2dffd23a6f
ryan-son <ryan.son1002@gmail.com>
2021-08-31 18:59:21 +0900

    feat: ViewPhotoButton 내 사진 속성 변경
    - 설정된 사진이 bounds에 맞춰 잘리도록 설정
    
    Fixes: #9

--- OpenMarket/OpenMarket/Sources/Views/MarketItemRegisterView/Components/ViewPhotoButton.swift
+++ OpenMarket/OpenMarket/Sources/Views/MarketItemRegisterView/Components/ViewPhotoButton.swift
@@ -11,22 +11,28 @@ final class ViewPhotoButton: UIButton {
 
     private enum Style {
 
+        static let borderColor: CGColor = UIColor.secondaryLabel.cgColor
+        static let borderWidth: CGFloat = 0.5
+        static let cornerRadius: CGFloat = 10
+
         enum DeleteButton {
-            static let image = UIImage(systemName: "xmark")
-            static let backgroundColor: UIColor = .label
-            static let tintColor: UIColor = .systemBackground
-            static let borderColor: UIColor = .systemBackground
+            static let image = UIImage(systemName: "xmark.circle.fill")
+            static let tintColor: UIColor = .label
+            static let backgroundColor: UIColor = .systemBackground
+            static let borderColor: CGColor = UIColor.systemBackground.cgColor
             static let borderWidth: CGFloat = 1.5
-            static let cornerRadius: CGFloat = 15
-            static let widthAgainstSuperview: CGFloat = 0.2
+            static let size: CGFloat = 25
         }
     }
 
     private let deleteButton: UIButton = {
         let button = UIButton()
         button.setBackgroundImage(Style.DeleteButton.image, for: .normal)
-        button.backgroundColor = Style.DeleteButton.backgroundColor
         button.tintColor = Style.DeleteButton.tintColor
+        button.layer.borderColor = Style.DeleteButton.borderColor
+        button.layer.borderWidth = Style.DeleteButton.borderWidth
+        button.backgroundColor = Style.DeleteButton.backgroundColor
+        button.layer.cornerRadius = Style.DeleteButton.size / 2
         button.clipsToBounds = true
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
@@ -34,9 +40,11 @@ final class ViewPhotoButton: UIButton {
 
     init(image: UIImage) {
         super.init(frame: .zero)
+        setStyle()
         setupViews()
         setupConstraints()
-        setBackgroundImage(image, for: .normal)
+        setImage(image, for: .normal)
+
     }
 
     @available(iOS, unavailable, message: "Use init(image:) instead")
@@ -44,15 +52,37 @@ final class ViewPhotoButton: UIButton {
         super.init(coder: coder)
     }
 
+    private func setStyle() {
+        layer.borderColor = Style.borderColor
+        layer.borderWidth = Style.borderWidth
+        layer.cornerRadius = Style.cornerRadius
+        imageView?.layer.cornerRadius = Style.cornerRadius
+        imageView?.contentMode = .scaleAspectFit
+        imageView?.clipsToBounds = true
+    }
+
     private func setupViews() {
         addSubview(deleteButton)
+        addBorder()
     }
 
     private func setupConstraints() {
         NSLayoutConstraint.activate([
-            deleteButton.widthAnchor.constraint(equalTo: widthAnchor,
-                                                multiplier: Style.DeleteButton.widthAgainstSuperview),
-            deleteButton.heightAnchor.constraint(equalTo: deleteButton.heightAnchor)
+            deleteButton.widthAnchor.constraint(equalToConstant: Style.DeleteButton.size),
+            deleteButton.heightAnchor.constraint(equalToConstant: Style.DeleteButton.size),
+            deleteButton.trailingAnchor.constraint(equalTo: trailingAnchor,
+                                                   constant: Style.DeleteButton.size / 2),
+            deleteButton.topAnchor.constraint(equalTo: topAnchor,
+                                                   constant: -Style.DeleteButton.size / 2)
         ])
+    }
+
+    private func addBorder() {
+        let layer = CALayer()
+        layer.frame = CGRect(x: .zero, y: .zero, width: frame.size.width, height: frame.size.height)
+        layer.borderColor = Style.borderColor
+        layer.borderWidth = Style.borderWidth
+        self.layer.addSublayer(layer)
+        deleteButton.layer.insertSublayer(layer, at: .zero)
     }
 }
